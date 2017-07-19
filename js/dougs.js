133484b73408086e4f0c52d04ab72e132d3830ab
Grant McGrath <grantmcgrath@me.com>
2017-07-19 17:37:10 -0400

    Small changes

--- js/dougs.js
+++ js/dougs.js
@@ -34,16 +34,5 @@ fetch("https://opendata.arcgis.com/datasets/124c2187da8c41c59bde04fa67eb2872_0.g
   });
 });
 
-// fetch("https://opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson").then(function(response){
-//   response.json().then(function(data){
-//     console.log(data);
-//
-//   });
-// });
-//
-// fetch("http://maps.wakegov.com/arcgis/rest/services/Inspections/RestaurantInspectionsOpenData/MapServer/2/query?where=1%3D1&outFields=HSISID,InspectDate,comments&outSR=4326&f=json").then(function(response){
-//   response.json().then(function(data){
-//     console.log(data);
-//
-//   });
-// });
+
+console.log(Date.now() - 33696000);
---
+++ js/main.js
@@ -0,0 +1,15 @@
+// fetch("https://opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson").then(function(response){
+//   response.json().then(function(data){
+//     console.log(data);
+//
+//   });
+// });
+//
+// fetch("http://maps.wakegov.com/arcgis/rest/services/Inspections/RestaurantInspectionsOpenData/MapServer/2/query?where=1%3D1&outFields=HSISID,InspectDate,comments&outSR=4326&f=json").then(function(response){
+//   response.json().then(function(data){
+//     console.log(data);
+//
+//   });
+// });
+
+// Above lines of code were from Grant's original testing
