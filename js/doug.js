0871a92f31ff0130f23eb5cc807de1ba4eb1eddb
LivingDevGirl <livingdevgirl@gmail.com>
2017-07-21 15:35:28 -0400

    last commit before results of hackathon

---
+++ js/doug.js
@@ -0,0 +1,39 @@
+
+let transformedData;
+
+fetch("https://opendata.arcgis.com/datasets/124c2187da8c41c59bde04fa67eb2872_0.geojson").then(function(response){
+  response.json().then(function(data){
+    //console.log(data);
+    transformedData = data.features.reduce((acc, restaurant) => {
+      // add the rest. to the acc
+      acc[restaurant.properties.HSISID] = restaurant.properties;
+      // inspections
+      acc[restaurant.properties.HSISID].inspections = [];
+      return acc;
+    }, );
+
+    // now go get our inspection data.
+    fetch("https://opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson")
+    .then(function(response){
+      if(response.status === 200){
+        //response.json().then(inspections => console.log(inspections.features[0].properties))
+
+        //response.json().then(inspections => console.log(inspections.features.filter(insp => insp.properties.HSISID === "04092011278")))
+        response.json().then(inspections => {
+          inspections.features.forEach(function(insp){
+            let hid = insp.properties.HSISID;
+            let restaurant = transformedData[hid];
+            if(!restaurant) return;
+            transformedData[hid].inspections.push(insp.properties);
+          })
+        })
+
+        console.log(transformedData);
+      }
+    })
+
+  });
+});
+
+
+console.log(Date.now() - 33696000);
---
+++ js/hidden.js
@@ -0,0 +1,6 @@
+// Hides the results and footer sections onload
+window.onload = function() {
+
+};
+
+// Shrinks the emptySection to a height of 0 to allow the search bar to rise
